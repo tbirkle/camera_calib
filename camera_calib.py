b65bb443ca62c0f33bc1ebafa8d88e883b7a447d
Tobias Birkle <tobirkle@htwg-konstanz.de>
2017-01-08 21:05:19 +0100

    fixed some errors. added validation data

--- camera_calib.py
+++ camera_calib.py
@@ -31,7 +31,12 @@ def temporal_variance(x):
     return variance
 
 def power_spectrum(fft, axis=0):
-    return np.sqrt((1 / fft.shape[~axis]) * np.sum((fft * np.conj(fft)), axis=~axis))
+    if axis==0:
+        other_axis = 1
+    elif axis==1:
+        other_axis = 0
+
+    return np.sqrt((1 / fft.shape[other_axis]) * np.sum((fft * np.conj(fft)), axis=other_axis))
 
 from scipy.ndimage import gaussian_filter
 from scipy.misc import imsave
@@ -53,26 +58,40 @@ def test(path):
 
 
 
+import skimage
 
 def read_dir(path):
     flat = os.listdir(path)
     images = []
     for filename in flat:
         if (filename.split(".")[1] == "png"):
-            img = scipy.misc.imread(os.path.join(path, filename))
+            img = scipy.misc.imread(os.path.join(path, filename), mode="I")
+            #img = scipy.misc.toimage(img, low=0, high=4095, mode="I")
+            #img = img.astype(np.uint16)
             images.append(img)
-
     images = np.asarray(images)
 
     return images
 
 
 
-flat_images = read_dir("flat")
-dark_images = read_dir("dark")
+flat_images = read_dir("mv")
+#dark_images = read_dir("dark")
+
+print(len(flat_images))
+
+dark1 = scipy.misc.imread(os.path.join("mv", "mv01-000a.png"), mode="I")
+dark2 = scipy.misc.imread(os.path.join("mv", "mv01-000b.png"), mode="I")
+dark_images = [dark1, dark2] * 51
+
+
+print(np.mean(flat_images, axis=(1,2)))
 
 mean_flat = averageEvery2(np.mean(flat_images, axis=(1,2)))
 mean_dark = averageEvery2(np.mean(dark_images, axis=(1,2)))
+
+print(mean_flat)
+print(mean_dark)
 
 var_flat = temporal_variance(flat_images)
 var_dark = temporal_variance(dark_images)
@@ -80,8 +99,13 @@ var_dark = temporal_variance(dark_images)
 exposure_times = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10 , 11, 12, 13, 14, 15])
 
 photons = number_photons(exposure_times)
+photons = []
+for i in range(51):
+    photons.append(i * 1659.66)
 
+photons = np.asarray(photons)
 
+"""
 max_idx = np.argmax(var_flat)
 print("Saturation", mean_flat[max_idx], max_idx)
 
@@ -93,27 +117,42 @@ reg_x = mean_reg_flat - mean_reg_dark
 reg_y = var_flat[:len(mean_reg_flat)] - var_dark[:len(mean_reg_flat)]
 regression = stats.linregress(reg_x, reg_y)
 slope = regression[0]
+"""
 
-K = slope
+mean_without_noise = mean_flat - mean_dark
+var_without_noise = var_flat - var_dark
+
+saturation_idx = np.argmax(var_without_noise)
+print("Saturation", mean_without_noise[saturation_idx], saturation_idx)
+
+saturation_start = mean_without_noise[saturation_idx] * 0.7
+
+regression_x = mean_without_noise[mean_without_noise < saturation_start]
+regression_y = var_without_noise[:len(regression_x)]
+
+print(regression_x)
+print(regression_y)
+
+regression = stats.linregress(regression_x, regression_y)
+
+K = regression[0]
 print("System gain K:", K)
 
 plt.figure(1)
 plt.title("Photon-Transfer-Curve")
-plt.plot([0,sat1],[regression[1],regression[1]+regression[0]*sat1])
-plt.plot(mean_flat - mean_dark, var_flat - var_dark)
-
+plt.plot([0,saturation_start],[regression[1],regression[1]+regression[0]*saturation_start])
+plt.plot(mean_without_noise, var_without_noise)
+plt.plot([mean_without_noise[saturation_idx], mean_without_noise[saturation_idx]],[var_without_noise[saturation_idx],0],"--")
 
 """Full-well capacity"""
-max_var = max(var_flat - var_dark)
+max_var = max(var_flat - var_dark)  # TODO ist das richtig?
 print("Full-Well:", max_var)
 
 
 
 
-
-
 """Sensitivity"""
-reg_y = mean_reg_flat - mean_reg_dark
+reg_y = mean_without_noise[mean_without_noise < saturation_start]
 reg_x = photons[:len(reg_y)]
 regression = stats.linregress(reg_x, reg_y)
 slope = regression[0]
@@ -124,8 +163,8 @@ print("Responsitivity R: ", R)
 
 plt.figure(2)
 plt.title("Sensitivity")
-plt.plot(photons, mean_flat-mean_dark)
-plt.plot([0,reg_x[len(reg_x)-1]],[regression[1], regression[1]+regression[0]*reg_x[len(reg_x)-1]], "-")
+plt.plot(photons, mean_without_noise)
+plt.plot([0,reg_x[len(reg_x)-1]],[regression[1], regression[1]+regression[0]*reg_x[len(reg_x)-1]], "r--")
 
 
 """Quantum Efficiency"""
@@ -133,20 +172,35 @@ QE = R / K
 print("Quantum Efficiency", QE)
 
 """Dark Noise"""
-dark_noise = (var_dark[0]-QUANTISATION_ERROR) / K**2
-print("Dark Noise: ", dark_noise)
+
+var_dark_temp = var_dark[0]
+"""
+if var_dark_temp < 0.24: # temporal noise is dominated by the quantization noise P.17 EMVA
+    var_dark_temp = 0.49
+    dark_noise = 0.4/K
+else:
+    dark_noise = np.sqrt(var_dark_temp-QUANTISATION_ERROR) / K
+"""
+
+if var_dark_temp < 0.24: # temporal noise is dominated by the quantization noise P.17 EMVA
+    var_dark_temp = 0.49
+
+var_dark_noise = (var_dark_temp - QUANTISATION_ERROR) / K**2
+
+print("Variance Dark Noise: ", var_dark_noise, "e")
+print("Std Dark Noise: ", np.sqrt(var_dark_noise), "e")
 
 """AUFGABE 1 ENDE"""
 
 
 """Signal to Noise ratio"""
-SNR_each_pic = (mean_flat-mean_dark)/np.sqrt(var_flat)  #SNR für jede Belichtungszeit
+SNR_each_pic = (mean_without_noise)/np.sqrt(var_flat)  #SNR für jede Belichtungszeit
 
 SNR_theor = (QE*photons)/np.sqrt(var_dark+(1/12/K**2)+QE*photons) #SNR aus den zuvor ermittelten Werten
 
 SNR_ideal = np.sqrt(photons) #SNR von idealem Sensor
 
-sat_photons = photons[len(mean_reg_flat)-1]
+sat_photons = photons[len(mean_without_noise[mean_without_noise < saturation_start])-1]
 
 photons_min = (1/QE) *((np.sqrt(var_dark[0])/K)+0.5) #Wie berechnet man var_dark ? Welche Bilder (Belichtungszeiten)?
 
@@ -161,8 +215,8 @@ ideal, = plt.plot(photons, SNR_ideal,"r",label="ideale SNR")
 theo, = plt.plot(photons, SNR_theor,"g", label="theoretische SNR")
 real, = plt.plot(photons, SNR_each_pic,"bx", label="reale SNR")
 #plt.plot[[sat_photons,0],[sat_photons,10**10]]
-plt.plot([sat_photons, sat_photons],[0, 100000000], "--")
-plt.plot([photons_min, photons_min],[0, 100000000], "--")
+plt.plot([sat_photons, sat_photons],[0, SNR_ideal.max()+100], "--")
+plt.plot([photons_min, photons_min],[0, SNR_ideal.max()+100], "--")
 plt.plot()
 
 plt.legend(handles=[ideal, theo, real])
@@ -179,7 +233,8 @@ print("Dynamikbereich: ", DR)
 
 """Ende Aufgabe 2"""
 
-flat50 = read_dir("50flat")
+#flat50 = read_dir("50flat")
+flat50 = read_dir("prnu")
 mean_flat50_image = np.mean(flat50, axis=0)
 mean_flat50 = mean_flat50_image.mean()
 size = mean_flat50_image.shape[0] * mean_flat50_image.shape[1]
@@ -189,18 +244,21 @@ variance_flat50 = (1/size) * (np.sum((mean_flat50_image - mean_flat50)**2))
 
 
 
-dark50 = read_dir("50dark")
+#dark50 = read_dir("50dark")
+dark50 = read_dir("dsnu")
 mean_dark50_image = np.mean(dark50, axis=0)
 mean_dark50 = mean_dark50_image.mean()
 size = mean_dark50_image.shape[0] * mean_dark50_image.shape[1]
 variance_dark50 = (1/size) * (np.sum((mean_dark50_image - mean_dark50)**2))
 
-temporal_variance_flat50_image = (1/(len(flat50)-1)) * np.sum(flat50 - mean_flat50_image,axis=0)
+temporal_variance_flat50_image = (1/(len(flat50)-1)) * np.sum((flat50 - mean_flat50_image)**2,axis=0)
 temporal_variance_flat50_stack = np.mean(temporal_variance_flat50_image)
 
-temporal_variance_dark50_image = (1/(len(dark50)-1)) * np.sum(dark50 - mean_dark50_image,axis=0)
-temporal_variance_dark50_stack = np.mean(temporal_variance_dark50_image)
+std_flat50_stack = np.sqrt(temporal_variance_flat50_stack)
 
+temporal_variance_dark50_image = (1/(len(dark50)-1)) * np.sum((dark50 - mean_dark50_image)**2,axis=0)
+temporal_variance_dark50_stack = np.mean(temporal_variance_dark50_image)
+std_dark50_stack = np.sqrt(temporal_variance_dark50_stack)
 
 DSNU = variance_dark50 / K
 PRNU = np.sqrt(variance_flat50 - variance_dark50) / (mean_flat50 - mean_dark50)
@@ -212,49 +270,53 @@ print("PRNU: ", PRNU, "%")
 
 
 
+
+
 # mean_flat50_image =  mean_flat50_image - mean_flat50
 mean_flat50_image_diff = mean_flat50_image - mean_flat50
 mean_dark50_image_diff = mean_dark50_image - mean_dark50
 
 """Spektrogramm PRNU Horizontal"""
-flat_fft_hor = np.fft.fft(mean_flat50_image_diff, axis=0)
+flat_fft_hor = (1/np.sqrt(mean_flat50_image_diff.size)) * np.fft.fft(mean_flat50_image_diff, axis=0)
 p_flat_hor = power_spectrum(flat_fft_hor, axis=0)
-plt.title("Spektrogramm PRNU Horizontal")
+
 plt.figure(4)
+plt.title("Spektrogramm PRNU Horizontal")
 plt.yscale("log")
 plt.plot(p_flat_hor[:len(p_flat_hor)//2])
-plt.plot([0, len(p_flat_hor)//2],[temporal_variance_flat50_stack,temporal_variance_flat50_stack], "g--")
+plt.plot([0, len(p_flat_hor)//2],[std_flat50_stack,std_flat50_stack], "g--")
 plt.plot([0, len(p_flat_hor)//2],[PRNU,PRNU], "r--")
 
 #plt.plot(range(len(p)//2), p[:len(p)//2])
 """Spektrogramm PRNU Vertikal"""
-flat_fft_vert = np.fft.fft(mean_flat50_image_diff, axis=1)
+
+flat_fft_vert = (1/np.sqrt(mean_flat50_image_diff.size)) * np.fft.fft(mean_flat50_image_diff, axis=1)
 p_flat_vert = power_spectrum(flat_fft_vert, axis=1)
 plt.figure(5)
 plt.title("Spektrogramm PRNU Vertikal")
 plt.yscale("log")
 plt.plot(p_flat_vert[:len(p_flat_vert)//2])
-plt.plot([0, len(p_flat_vert)//2],[temporal_variance_flat50_stack,temporal_variance_flat50_stack], "g--")
+plt.plot([0, len(p_flat_vert)//2],[std_flat50_stack,std_flat50_stack], "g--")
 plt.plot([0, len(p_flat_vert)//2],[PRNU,PRNU], "r--")
 
 """Spektrogramm DSNU Horizontal"""
-dark_fft_hor = np.fft.fft(mean_dark50_image_diff, axis=0)
+dark_fft_hor = (1/np.sqrt(mean_dark50_image_diff.size)) * np.fft.fft(mean_dark50_image_diff, axis=0)
 p_dark_hor = power_spectrum(dark_fft_hor, axis=0)
 plt.figure(6)
 plt.title("Spektrogramm DSNU Horizontal")
 plt.yscale("log")
 plt.plot(p_dark_hor[:len(p_dark_hor)//2])
-plt.plot([0, len(p_dark_hor)//2],[temporal_variance_dark50_stack,temporal_variance_dark50_stack], "g--")
+plt.plot([0, len(p_dark_hor)//2],[std_dark50_stack,std_dark50_stack], "g--")
 plt.plot([0, len(p_dark_hor)//2],[DSNU,DSNU], "r--")
 
 """Spektrogramm DSNU Vertikal"""
-dark_fft_vert = np.fft.fft(mean_dark50_image_diff, axis=1)
+dark_fft_vert = (1/np.sqrt(mean_dark50_image_diff.size)) * np.fft.fft(mean_dark50_image_diff, axis=1)
 p_dark_vert = power_spectrum(dark_fft_vert, axis=1)
 plt.figure(7)
 plt.title("Spektrogramm DSNU Vertikal")
 plt.yscale("log")
 plt.plot(p_dark_vert[:len(p_dark_vert)//2])
-plt.plot([0, len(p_dark_vert)//2],[temporal_variance_dark50_stack,temporal_variance_dark50_stack], "g--")
+plt.plot([0, len(p_dark_vert)//2],[std_dark50_stack,std_dark50_stack], "g--")
 plt.plot([0, len(p_dark_vert)//2],[DSNU,DSNU], "r--")
 
 """ENDE AUFGABE3"""
